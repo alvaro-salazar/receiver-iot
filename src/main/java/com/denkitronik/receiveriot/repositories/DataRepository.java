de19f59a23708420fc37c98db0cdb7a016dcbef3
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-11-03 16:25:30 -0500

    Fix: remove unused import

--- src/main/java/com/denkitronik/receiveriot/repositories/DataRepository.java
+++ src/main/java/com/denkitronik/receiveriot/repositories/DataRepository.java
@@ -2,7 +2,6 @@ package com.denkitronik.receiveriot.repositories;
 
 import com.denkitronik.receiveriot.entities.DataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 /**
  * Repositorio JPA para la entidad que representa los datos de las mediciones de los dispositivos
